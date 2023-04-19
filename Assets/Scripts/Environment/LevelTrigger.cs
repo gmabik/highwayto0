2a5ecf3c22b168dd0cf1e9e75e3bf6769a1f101b
GmaB-a <vladik11012006@gmail.com>
2023-04-19 13:38:44 +0200

    level generation on triggers done
    + sections as prefabs
    + small fixes

---
+++ Assets/Scripts/Environment/LevelTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //if (other.gameObject.tag != "Player") return;
+        GenerateLevel.Instance.GenerateSection();
+        Destroy(gameObject);
+    }
+}

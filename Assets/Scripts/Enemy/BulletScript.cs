94bb3e34db9d4725066ceff8e18dde5d7639f29c
Mxgalodon <116174094+Mxgalodon@users.noreply.github.com>
2023-05-03 21:14:12 +0200

    save
    zapomniałem tego zapisać XD

--- Assets/Scripts/Enemy/BulletScript.cs
+++ Assets/Scripts/Enemy/BulletScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject, 5f);
+        }
     }
 }
